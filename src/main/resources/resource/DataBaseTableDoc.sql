d859111233c27371c5e4bb45f2824876cc66b725
sizq <zq_soft@foxmail.com>
2020-10-15 10:21:46 +0800

    更新

---
+++ src/main/resources/resource/DataBaseTableDoc.sql
@@ -0,0 +1,57 @@
+
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (1001, 0, '缺货情况', b'0');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (1002, 0, '店铺授权', b'0');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (1003, 0, '订单追踪', b'0');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (1004, 0, '物流相关', b'0');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (1005, 0, '售后处理', b'0');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (1006, 0, '消息处理', b'0');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (1007, 0, '采购跟踪', b'0');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (1008, 0, '仓库相关', b'0');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (1009, 0, '财务相关', b'0');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (1010, 0, '报关跟踪', b'0');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20020, 1001, '缺货商品', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20021, 1002, '授权失效店铺', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20022, 1002, '未授权店铺', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20023, 1002, '未开启订单自动下载店铺', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20024, 1003, '待审核订单', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20025, 1003, '自有仓待配货订单', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20027, 1003, '自有仓24小时内待发货订单', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20028, 1003, '自有仓待发货订单', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20029, 1003, '自有仓上传失败订单', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20030, 1003, '第三方仓出库创建状态订单', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20031, 1003, '第三方仓发货上传失败订单', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20051, 1003, '系统订单发货情况', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20052, 1003, '超时未发货订单', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20026, 1004, '订单物流下单情况', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20034, 1004, '海外仓头程逾期未到货计划单', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20032, 1005, '售后处理中订单', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20033, 1006, '站内信未回复', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20054, 1006, '站内信未读', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20035, 1007, '采购逾期未交货单据', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20036, 1007, '待转采购的计划单', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20038, 1007, '待转采购入库的质检单', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20039, 1007, '待转采购入库的采购订单', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20048, 1007, '采购待审核单据', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20049, 1007, '采购质检情况', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20040, 1008, '第三方仓入库异常单据', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20041, 1008, '调拨逾期未到货单据', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20042, 1008, '调拨差异待处理商品', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20043, 1008, '头程差异待处理商品', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20044, 1008, '海外仓头程入库异常出库单 ', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20046, 1008, '发运后超过x天未发货确认的单据', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20050, 1008, '仓库待审核单据', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20045, 1009, '头程费用=0的头程订单', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20053, 1009, '低利润预警订单数', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20037, 1009, '待申请付款采购订单', b'1');
+INSERT INTO ecs_erp_message(id, parent_id, text, leaf) VALUES (20047, 1010, '待报关单据', b'1');
+
+
+
+
+
+
+
+
+
+
+
